ef94928f5b3657676919fca3d38a428ca8e6ad84
henry <henry@henrys-laptop.local>
2022-10-31 12:33:36 +0000

    simple working ok

--- src/chronos/TimerSimple.js
+++ src/chronos/TimerSimple.js
@@ -2,64 +2,36 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import PubSub from 'pubsub-js';
 import topics from '../pub/topics';
+import { getAudioSrc, timeToSeconds, dateTohms } from '../Utils';
 /*
 ok, code doesnt handle minutes, seconds, but it fucking works!
 see if can replace interval with heart beat.
 fix hms -> s.
 display h m s
+    
+NXT:
+    get seconds from timer.√
 */
 
 const Timer = ({ timer }) => {
   //   const { startingMinutes = 111, startingSeconds = 0 } = props;
-  console.log(timer);
-  const [mins, setMinutes] = useState(1); //timer.timer.m  '' -
-  const [secs, setSeconds] = useState(5);
 
-  //   useEffect(() => {
-  //     let sampleInterval = setInterval(() => {
-  //       if (secs > 0) {
-  //         setSeconds(secs - 1);
-  //       }
-  //       if (secs === 0) {
-  //         if (mins === 0) {
-  //           clearInterval(sampleInterval);
-  //         } else {
-  //           setMinutes(mins - 1);
-  //           setSeconds(59);
-  //         }
-  //       }
-  //     }, 1000);
-  //     return () => {
-  //       clearInterval(sampleInterval);
-  //     };
-  //   });
+  const [secs, setSeconds] = useState(
+    timeToSeconds(timer.timer.h, timer.timer.m, timer.timer.s)
+  );
+
+  useEffect(() => {
+    console.log(timer);
+    return () => {};
+  });
+
   var HeartBeatSubscriber = function (msg, data) {
-    if (secs > 0) {
-      setSeconds(secs - 1);
-    }
-    if (secs === 0) {
-      if (mins === 0) {
-        //clearInterval(sampleInterval);
-      } else {
-        setMinutes(mins - 1);
-        setSeconds(59);
-      }
-    }
+    setSeconds(secs - 1);
   };
+
   PubSub.subscribe(topics.HEARTBEAT, HeartBeatSubscriber);
 
-  return (
-    <div>
-      {!(mins && secs) ? (
-        ''
-      ) : (
-        <p>
-          {' '}
-          {mins}:{secs < 10 ? `0${secs}` : secs}
-        </p>
-      )}
-    </div>
-  );
+  return <div>{secs < 10 ? `0${secs}` : secs}</div>;
 };
 
 export default Timer;
